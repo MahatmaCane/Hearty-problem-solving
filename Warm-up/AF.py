8b6b801557501521d6bdb71ef4528abbbf502eee
MahatmaCane <MahatmaCane@users.noreply.github.com>
2016-07-30 16:10:59 +0100

    # This is a combination of 12 commits. # The first commit's message is: Add files via upload
    # The 2nd commit message will be skipped:
    
    #	Rename AF.py to Warm-up/AF.py
    
    # The 3rd commit message will be skipped:
    
    #	Rename AFModel.py to Warm-up/AFModel.py
    
    # The 4th commit message will be skipped:
    
    #	Renamed Substrate class as Myocardium
    
    # The 5th commit message will be skipped:
    
    #	Fixed typo
    
    # The 6th commit message will be skipped:
    
    #	Added basic command line interface
    
    # The 7th commit message will be skipped:
    
    #	Animation switch added
    
    # The 8th commit message will be skipped:
    
    #	Animation switch added, bugs fixed
    
    # The 9th commit message will be skipped:
    
    #	Ablation functionality via click and drag added
    
    # The 10th commit message will be skipped:
    
    #	Renamed variable s as myocardium
    
    # The 11th commit message will be skipped:
    
    #	Added Electrogram class to record activity.
    
    # The 12th commit message will be skipped:
    
    #	Pickles electrogram numpy array, number of active cells method in Myocardium

---
+++ Warm-up/AF.py
@@ -0,0 +1,183 @@
+#### Copyright Joel Dyer, 29/07/2016 ####
+
+import argparse
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+import pickle
+import time
+
+from AFModel import Myocardium
+
+class Electrogram:
+
+    """Records number of activated cells at each time step."""
+
+    def __init__(self):
+        self.activity = []
+        self.time = []
+
+    def record(self, time, activity):
+        self.time.append(time)
+        self.activity.append(activity)
+
+class Ablater:
+
+    """Ablates specified region of myocardium."""
+
+    def __init__(self, one_corner):
+        self.first_corner = one_corner
+
+    def set_final_corner(self, corner):
+        self.final_corner = corner
+
+class TimeTracker:
+
+    """Steps time forwards & controls termination of simulation when
+       animating."""
+
+    def __init__(self, tinit=0, tmax=np.inf):
+
+        self.tmax = tmax
+        self.tinit = tinit
+
+    def __iter__(self):
+
+        t = self.tinit
+        while t < self.tmax:
+            yield t
+            if self.stop is True:
+                raise StopIteration()
+            t += 1
+
+    def stop(self):
+
+        self.stop = True
+
+
+def state_pickler(out_dir, myocardium, random_state, t): 
+
+    info = {'myo':myocardium, 'rand_state':random_state, 'time':t}
+    with open(out_dir + '/State-{0}-{1}'.format(myocardium._nu, t), 'w') as fh:
+        pickle.dump(info, fh)
+
+
+def run(tmax=1e3, heart_rate=220, tissue_shape=(200, 200), nu=0.8, d=0.05,
+        e=0.05, ref_period=50, animate=True, out_dir=False, plot_egram=False,
+        pickle_frequency=None, state_file=None):
+
+    args = {'heart rate':heart_rate, 'epsilon':e, 'delta':d, 'tmax':tmax, 
+            'tau':ref_period, 'nu':nu, 'shape':tissue_shape}
+
+    if state_file is not None:
+        with open(state_file, 'r') as fh:
+            state_dict = pickle.load(fh)
+        myocardium = state_dict['myo']
+        np.random.set_state(state_dict['rand_state'])
+        tt = TimeTracker(tinit=state_dict['time'], tmax=tmax)
+    else:
+        myocardium = Myocardium(tissue_shape, nu, d, e, ref_period)
+        tt = TimeTracker(tmax=tmax)
+
+    if animate == True:
+        ax = plt.gca()
+        fig = ax.get_figure()
+        myo_state = myocardium.counts_until_relaxed
+        qm = ax.pcolorfast(myo_state, cmap='Greys_r', vmin=0, vmax=ref_period)
+        qm.set_array(myocardium.counts_until_relaxed)
+        plt.draw()
+        plt.pause(0.0001)
+        
+        textstr = 'Tissue Shape$={0}$ \n'.format(tissue_shape) + r'$\nu$' 
+        textstr += '$={0}$ \n$\delta={1}$'.format(nu, d)
+        textstr += '\nRefractory Period$={0}$'.format(ref_period)
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.4)
+        ax.text(0.75, 0.15, textstr, transform=ax.transAxes, fontsize=8,
+                verticalalignment='top', bbox=props)
+
+        def handle_close(evt):
+            tt.stop()
+
+        def store_corner(evt):
+            global ablater
+            ablater = Ablater((evt.xdata, evt.ydata))
+
+        def ablate(evt):
+            ablater.set_final_corner((evt.xdata, evt.ydata))
+            myocardium.render_unexcitable(ablater.first_corner,
+                                          ablater.final_corner)
+
+        fig.canvas.mpl_connect('close_event', handle_close)
+        fig.canvas.mpl_connect('button_press_event', store_corner)
+        fig.canvas.mpl_connect('button_release_event', ablate)
+    else:
+        print "Beginning simulation with parameters: {}.".format(args)
+
+    if out_dir is not False:
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        egram = Electrogram()
+        egram.record(0, myocardium.number_of_active_cells())
+
+    for time in tt:
+        if out_dir is not False:
+            if pickle_frequency is not None:
+                if time%pickle_frequency == 0:
+                   rand_state = np.random.get_state()
+                   state_pickler(out_dir, myocardium, rand_state, time)
+
+        if time%heart_rate == 0:
+            myocardium.evolve(pulse=True)
+        else:
+            myocardium.evolve()
+
+        if animate == True:
+            qm.set_array(myocardium.counts_until_relaxed)
+            count = myocardium.number_of_active_cells()
+            ax.set_title('Time: {0}, Active Cells: {1}'.format(time, count))
+            plt.draw()
+            plt.pause(0.0001)
+
+        if out_dir is not False:
+            egram.record(time, myocardium.number_of_active_cells())
+
+    if out_dir is not False:
+        with open(out_dir + '/Run-{0}'.format(nu),'w') as fh:
+            pickle.dump(np.array([egram.time, egram.activity]), fh)
+            
+    if plot_egram == True:
+        fig = plt.figure(2)
+        plt.scatter(egram.time, egram.activity)
+        plt.title('Total Cell Activity')
+        plt.show()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--shape', '-s', type=int, default=200,
+                        help='Myocardium dimensions.')
+    parser.add_argument('--nu', '-n', type=float, default=0.21,
+                        help='Fraction of existing lateral couplings.')
+    parser.add_argument('--delta', '-d', type=float, default=0.05,
+                        help='Fraction of defective cells.')
+    parser.add_argument('--epsilon', '-e', type=float, default=0.95,
+                        help='Probability that defective cell fails to fire.')
+    parser.add_argument('--animate', '-a', action='store_true', 
+                        help='Animation switch for live animation.')
+    parser.add_argument('--out_dir', '-o', type=str, default=False,
+                        help='Output directory for data dumping.')
+    parser.add_argument('--plot_egram', '-p', action='store_true',
+                        help="""Switch for plotting electrogram activity\n 
+                              against time.""")
+    parser.add_argument('--pickle_frequency', '-f', type=int, default=None,
+                        help="""Inverse frequency at which to pickle state of\n
+                              the simulations. Requires a specified out_dir.""")
+    parser.add_argument('--state_file', '-m', type=str, default=None,
+                        help='Pickle file containing pickled myocardium.')
+    args = parser.parse_args()
+
+    run(nu=args.nu, d=args.delta, e=args.epsilon, animate=args.animate,
+        tissue_shape=(200, args.shape), out_dir=args.out_dir,
+        pickle_frequency=args.pickle_frequency, state_file=args.state_file)
---
+++ Warm-up/AFModel.py
@@ -0,0 +1,87 @@
+#### Copyright Joel Dyer, 29/07/2016 ####
+
+import numpy as np
+
+class Myocardium:
+
+    def __init__(self, tissue_shape=(5, 5), nu=0.5, d=0.2, e=0.4, refractory_period=5):
+
+        self.shape = tissue_shape
+        self._nu = nu
+        self._delta = d
+        self._epsilon = e
+        self._refractory_period = refractory_period
+
+        self.__determine_locations_of_lateral_couplings()
+        self.__determine_locations_of_defective_cells()
+
+        self.counts_until_relaxed = np.zeros(self.shape, dtype=np.float64)
+        self.wavefront = np.zeros(self.shape, dtype=np.bool)
+        self.permanently_unexcitable = np.zeros(tissue_shape)
+
+    def __determine_locations_of_lateral_couplings(self):
+
+        random = np.random.random(self.shape)
+        self.lateral_couplings = random <= self._nu
+
+    def __determine_locations_of_defective_cells(self):
+
+        random = np.random.random(self.shape)
+        self._defective_cells = random <= self._delta
+
+    def __find_cells_to_excite(self):
+
+        """Determines cells which are to excite in response to stimulus in the
+        next time step."""
+
+        # Four possible situations: cell completely relaxed & not defective;
+        # cell completely relaxed and defective; cell ablated (permanently
+        # unexcitable); cell still refractory (temporarily unexcitable).
+
+        self.excitable_cells = np.ones(self.shape)
+
+        # Defective
+        self.excitable_cells[self._defective_cells] = np.random.random(np.sum(self._defective_cells))
+        self.excitable_cells = self.excitable_cells > self._epsilon
+        # Refractory
+        self.excitable_cells[self.counts_until_relaxed != 0] = False
+        # Ablated
+        self.excitable_cells[self.permanently_unexcitable == 1] = False
+        
+        # Remaining cells are relaxed and have no reason to not excite given
+        # a stimulus
+
+    def __update_counts_until_relaxed(self):
+
+        refractory_cells = self.counts_until_relaxed > 0
+        self.counts_until_relaxed[refractory_cells] -= 1
+        self.counts_until_relaxed[self.wavefront] = self._refractory_period 
+ 
+    def evolve(self, pulse=False):
+
+        self.__find_cells_to_excite()
+
+        new_wavefront = np.roll(self.wavefront * self.lateral_couplings, -1, axis=0)
+        new_wavefront += np.roll(self.wavefront, 1, axis=0) * self.lateral_couplings
+        new_wavefront[:, :-1] += self.wavefront[:, 1:]
+        new_wavefront[:, 1:] += self.wavefront[:, :-1]
+        new_wavefront *= self.excitable_cells
+
+        if pulse is True:
+            new_wavefront[:, 0] += self.excitable_cells[:, 0]
+
+        self.wavefront = new_wavefront
+
+        self.__update_counts_until_relaxed()
+
+    def render_unexcitable(self, one_corner, another_corner):
+        
+        little_x = int(min(one_corner[0], another_corner[0]))
+        big_x = int(max(one_corner[0], another_corner[0]))
+        little_y = int(min(one_corner[1], another_corner[1]))
+        big_y = int(max(one_corner[1], another_corner[1]))
+        self.permanently_unexcitable[little_y:big_y, little_x:big_x] = 1
+
+    def number_of_active_cells(self):
+
+        return np.sum(self.wavefront)
