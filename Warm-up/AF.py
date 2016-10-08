36037e8ada7c465b876b1e6790ff88b282ab8e95
MahatmaCane <Joel@dyn1230-159.wlan.ic.ac.uk>
2016-10-08 16:11:53 +0100

    Animation switch added

--- Warm-up/AF.py
+++ Warm-up/AF.py
@@ -9,59 +9,60 @@ from AFModel import Myocardium
 
 class TimeTracker:
 
-	def __init__(self, tmax=np.inf):
+    def __init__(self, tmax=np.inf):
 
-		self.tmax = tmax
-		self.tinit = 0
+        self.tmax = tmax
+        self.tinit = 0
 
-	def __iter__(self):
-		
-		t = 1
-		while t < self.tmax:
-			yield t
-			if self.stop is True:
-				raise StopIteration()
-			t += 1
+    def __iter__(self):
 
-	def stop(self):
+        t = 1
+        while t < self.tmax:
+            yield t
+            if self.stop is True:
+                raise StopIteration()
+            t += 1
 
-		self.stop = True
+    def stop(self):
+
+        self.stop = True
 
 def run(tmax=np.inf, heart_rate=250, tissue_shape=(200, 200), nu=0.8, 
-        delta=0.05, p=0.95, refractory_period=50):
+        delta=0.05, p=0.95, refractory_period=50, animate=True):
 
-	ax = plt.gca()
-	s = Myocardium(tissue_shape, nu, delta, p, refractory_period)
-	qm = ax.pcolorfast(s.counts_until_relaxed)
-	qm.set_array(s.counts_until_relaxed)
-	ax.set_title('0')
-	tt = TimeTracker()
-	plt.draw()
-	plt.pause(0.01)
+    s = Myocardium(tissue_shape, nu, delta, p, refractory_period)
+    tt = TimeTracker()
 
-	fig = ax.get_figure()
+    if animate == True:
+        ax = plt.gca()
+        fig = ax.get_figure()
+        qm = ax.pcolorfast(s.counts_until_relaxed)
+        qm.set_array(s.counts_until_relaxed)
+        ax.set_title('0')
+        plt.draw()
+        plt.pause(0.01)
 
-	def handle_close(evt):
-		tt.stop()
+        def handle_close(evt):
+            tt.stop()
 
-	fig.canvas.mpl_connect('close_event', handle_close)
+        fig.canvas.mpl_connect('close_event', handle_close)
+    else:
+        print "Beginning simulation."
 
-	for time in tt:
-		s.evolve_wavefront()
-		s.update_counts_until_relaxed()
-		if time%heart_rate == 0:
-			s.pulse()
-			qm.set_array(s.counts_until_relaxed)
-			ax.set_title('{}'.format(time))
-			plt.draw()
-		else:
-			qm.set_array(s.counts_until_relaxed)
-			ax.set_title('{}'.format(time))
-			plt.draw()
-		plt.pause(0.01)
+    for time in tt:
+        s.evolve_wavefront()
+        s.update_counts_until_relaxed()
+        if time%heart_rate == 0:
+            s.pulse()
+
+        if animate == True:
+            qm.set_array(s.counts_until_relaxed)
+            ax.set_title('{}'.format(time))
+            plt.draw()
+            plt.pause(0.01)
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--shape', '-s', type=int, default=200,
@@ -71,6 +72,8 @@ if __name__ == "__main__":
                         help='delta')
     parser.add_argument('--epsilon', '-e', type=float, default=0.95,
                         help='epsilon')
+    parser.add_argument('--animate', '-a', action='store_true')
     args = parser.parse_args()
-    
-    run(nu=args.nu, delta=args.delta, p=args.epsilon, tissue_shape=(200, args.shape))
+
+    run(nu=args.nu, delta=args.delta, p=args.epsilon, animate=args.animate,
+        tissue_shape=(200, args.shape))
--- Warm-up/AFModel.py
+++ Warm-up/AFModel.py
@@ -5,69 +5,69 @@ import random
 
 class Myocardium:
 
-	def __init__(self, tissue_shape=(5, 5), nu=0.5, delta=0.2, p=0.4, refractory_period=5):
+    def __init__(self, tissue_shape=(5, 5), nu=0.5, delta=0.2, p=0.4, refractory_period=5):
 
-		assert isinstance(tissue_shape, tuple), 'Expected tuple, got {}'.format(type(tissue_shape))
+        assert isinstance(tissue_shape, tuple), 'Expected tuple, got {}'.format(type(tissue_shape))
         self.shape = tissue_shape
-		self._nu = nu
-		self._delta = delta
-		self._p = p
-		self._refractory_period = refractory_period
+        self._nu = nu
+        self._delta = delta
+        self._p = p
+        self._refractory_period = refractory_period
 
-		self.determine_locations_of_lateral_couplings()
-		self.determine_locations_of_defective_cells()
+        self.determine_locations_of_lateral_couplings()
+        self.determine_locations_of_defective_cells()
 
-		self.counts_until_relaxed = np.zeros(self.shape)
-		self.wavefront = np.zeros(self.shape)
-		self.pulse()
-		
-		self.find_excitable_cells()
+        self.counts_until_relaxed = np.zeros(self.shape)
+        self.wavefront = np.zeros(self.shape)
+        self.pulse()
+        
+        self.find_excitable_cells()
 
-	def determine_locations_of_lateral_couplings(self):
+    def determine_locations_of_lateral_couplings(self):
 
-		random = np.random.random(self.shape)
-		self.lateral_couplings = random <= self._nu
+        random = np.random.random(self.shape)
+        self.lateral_couplings = random <= self._nu
 
-	def determine_locations_of_defective_cells(self):
+    def determine_locations_of_defective_cells(self):
 
-		random = np.random.random(self.shape)
-		self._defective_cells = random <= self._delta
+        random = np.random.random(self.shape)
+        self._defective_cells = random <= self._delta
 
-	def find_excitable_cells(self):
+    def find_excitable_cells(self):
 
-		self.excitable_cells = self.counts_until_relaxed == 0
+        self.excitable_cells = self.counts_until_relaxed == 0
 
-	def pulse(self):
+    def pulse(self):
 
-		self.find_excitable_cells()
-		ones = self.find_cells_which_will_fire()
-		self.wavefront[:, 0] += ones[:, 0] * self.excitable_cells[:, 0]
-		self.counts_until_relaxed[:,0] += ones[:, 0] * self.excitable_cells[:, 0] * self._refractory_period
+        self.find_excitable_cells()
+        ones = self.find_cells_which_will_fire()
+        self.wavefront[:, 0] += ones[:, 0] * self.excitable_cells[:, 0]
+        self.counts_until_relaxed[:,0] += ones[:, 0] * self.excitable_cells[:, 0] * self._refractory_period
 
-	def update_counts_until_relaxed(self):
+    def update_counts_until_relaxed(self):
 
-		refractory_cells = self.counts_until_relaxed > 0
-		self.counts_until_relaxed[refractory_cells] -= 1
-		self.counts_until_relaxed += self.wavefront * self._refractory_period 
+        refractory_cells = self.counts_until_relaxed > 0
+        self.counts_until_relaxed[refractory_cells] -= 1
+        self.counts_until_relaxed += self.wavefront * self._refractory_period 
 
-	def evolve_wavefront(self):
+    def evolve_wavefront(self):
 
-		should_excite = np.zeros(self.shape, dtype=np.float)
-		self.find_excitable_cells()
-		should_excite[:, :-1] += self.wavefront[:, 1:] * self.excitable_cells[:, :-1]
-		should_excite[:, 1:] += self.wavefront[:, :-1] * self.excitable_cells[:, 1:]
-		should_excite += np.roll(self.wavefront, -1, axis=0) * self.lateral_couplings * self.excitable_cells
-		should_excite += np.roll(self.wavefront, 1, axis=0) * self.lateral_couplings * self.excitable_cells
-		should_excite[should_excite > 1] = 1
+        should_excite = np.zeros(self.shape, dtype=np.float)
+        self.find_excitable_cells()
+        should_excite[:, :-1] += self.wavefront[:, 1:] * self.excitable_cells[:, :-1]
+        should_excite[:, 1:] += self.wavefront[:, :-1] * self.excitable_cells[:, 1:]
+        should_excite += np.roll(self.wavefront, -1, axis=0) * self.lateral_couplings * self.excitable_cells
+        should_excite += np.roll(self.wavefront, 1, axis=0) * self.lateral_couplings * self.excitable_cells
+        should_excite[should_excite > 1] = 1
 
-		ones = self.find_cells_which_will_fire()
+        ones = self.find_cells_which_will_fire()
 
-		self.wavefront = ones * should_excite
+        self.wavefront = ones * should_excite
 
-	def find_cells_which_will_fire(self):
+    def find_cells_which_will_fire(self):
 
-		ones = np.ones(self.shape)
-		ones[self._defective_cells] = np.random.random(np.sum(self._defective_cells))
-		ones[ones <= self._p] = 0
-		ones[ones > self._p] = 1
-		return ones
+        ones = np.ones(self.shape)
+        ones[self._defective_cells] = np.random.random(np.sum(self._defective_cells))
+        ones[ones <= self._p] = 0
+        ones[ones > self._p] = 1
+        return ones
