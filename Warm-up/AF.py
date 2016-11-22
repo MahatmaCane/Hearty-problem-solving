298a7c7445f611f3e6b7204a5af38ad6e654df00
System Administrator <root@dyn1231-158.wlan.ic.ac.uk>
2016-11-22 16:08:11 +0000

    Moved objects that we consider to be tools to our new module AFTools.py

--- Warm-up/AF.py
+++ Warm-up/AF.py
@@ -10,52 +10,6 @@ import time
 
 from AFModel import Myocardium
 
-class Electrogram:
-
-    """Records number of activated cells at each time step."""
-
-    def __init__(self):
-        self.activity = []
-        self.time = []
-
-    def record(self, time, activity):
-        self.time.append(time)
-        self.activity.append(activity)
-
-class Ablater:
-
-    """Ablates specified region of myocardium."""
-
-    def __init__(self, one_corner):
-        self.first_corner = one_corner
-
-    def set_final_corner(self, corner):
-        self.final_corner = corner
-
-class TimeTracker:
-
-    """Steps time forwards & controls termination of simulation when
-       animating."""
-
-    def __init__(self, tinit=0, tmax=np.inf):
-
-        self.tmax = tmax
-        self.tinit = tinit
-
-    def __iter__(self):
-
-        t = self.tinit
-        while t < self.tmax:
-            yield t
-            if self.stop is True:
-                raise StopIteration()
-            t += 1
-
-    def stop(self):
-
-        self.stop = True
-
-
 def state_pickler(out_dir, myocardium, random_state, t): 
 
     info = {'myo':myocardium, 'rand_state':random_state, 'time':t}
